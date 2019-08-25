2952ae7722fc86bae2a0e39c71ed0b21e0b524e9
creatorSeraph <dbgksdl00@gmail.com>
2019-08-26 07:01:27 +0900

    ThreadPool을 만들어보자

---
+++ ThreadPool.cpp
@@ -0,0 +1,41 @@
+#include "DXUT.h"
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(int threadSize)
+	:m_isPaused(false), m_isDestroy(false)
+{
+	m_threads.reserve(threadSize);//미리 vector의 공간을 잡아두는 역할을 한다. 재할당이 일어나지 않게 방지할수 있다.
+	for (int i = 0; i < threadSize; ++i)
+	{
+		m_threads.emplace_back([this]() {//this를 캡쳐하면 이 클래스 내부의 변수는 모두 다룰수 있게 된다.
+			while (true)
+			{
+				if (m_funcQueue.empty() && m_isDestroy) return;//할일도 없고 이제 Pool이 삭제 될때가 되었으면 thread도 일을 그만두자..
+				if (m_funcQueue.empty() || m_isPaused) continue;//일이 없거나 일시정지 상태일때는 굳이 mutex를 걸일도 없이 대기상태에 있는다.
+
+				unique_lock lock(m_mutex);//뭔가 일이 생겼나보다 일단 문부터 잠구고 시작하자
+				//lock_gurad는 무조건 소멸자에서만 풀수 있지만 lock_guard는 unlock을 호출할수 있다.
+
+				auto targetFunc = std::move(m_funcQueue.front());
+				m_funcQueue.pop();//원하는 함수를 찾아서 빼고 나면
+
+				lock.unlock();//이제 문닫고 나와서 일해야지
+
+				targetFunc();
+			}
+		});
+	}
+}
+
+ThreadPool::~ThreadPool()
+{
+	m_isDestroy = true;
+	//auto&에서 &를 빼면 나오는 소리는 thread의 복사생성자가 없기 때문에 나오는것. 범위기반 for문에서 &가 안붙어있으면 복사생성자를 호출한다는 뜻이다.
+	for (auto& iter : m_threads)//이미 실행중인 스레드들이 모두 종료될때까지 기다려야한다.(파일이 수정중이다가 프로그램이 강제종료되면..?)
+		iter.join();
+}
+
+void ThreadPool::AddFunc(std::function<void()> func)
+{
+	m_funcQueue.push(std::move(func));
+}
---
+++ ThreadPool.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <thread>
+#include <queue>
+#include <mutex>
+
+class ThreadPool
+{
+private:
+	vector<thread> m_threads;
+	queue<std::function<void()>> m_funcQueue;//먼저 들어온 함수가 먼저 실행되는 자료구조를 사용하기 위함
+	bool m_isPaused;//일시정지를 할 이유가 있을지 모르겠네
+	bool m_isDestroy;//threadPool이 사라질때 다른함수들에 알려줄 방법이 필요하다
+
+	mutable mutex m_mutex;
+public:
+	//threadSize : 사용할 스레드의 개수
+	//기본값 : hardware_concurrency : 논리 프로세서 개수
+	ThreadPool(int threadSize = thread::hardware_concurrency());
+	~ThreadPool();
+public:
+	void AddFunc(std::function<void()> func);
+	//이미 실행중인 함수를 제외한 이후 함수들은 작동을 일시정지 한다
+	inline void Pause() { m_isPaused = true; }
+	//일시정지 상태를 해제한다
+	inline void Resume() { m_isPaused = false; }
+	//이미 실행중인 함수를 제외한 이후 함수들을 제거한다
+	//void Reset();
+	//이미 실행중인 함수를 detach하고 이후 함수들을 제거한다
+	//void Reset();
+public:
+	inline bool isEmpty() const { return m_funcQueue.empty(); }
+	inline bool isPaused() const { return m_isPaused; }
+};
+

9c7b20c0e6f840eca39bd5535875c65368cbcc59
creatorSeraph <dbgksdl00@gmail.com>
2019-08-27 05:34:45 +0900

    Future를 사용한 함수 반환값 받아오기 기능

--- TestScene.cpp
+++ TestScene.cpp
@@ -11,28 +11,36 @@ TestScene::~TestScene()
 
 void TestScene::Init()
 {
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i < loopCount; ++i)
 	{
-		//m_tp.AddFunc([i]() {
-		//	cout << i << endl;
-		//});
+		m_tp.AddFunc([i]() {
+			cout << i << endl;
+		});
 
-		auto func = [](int val) {
+		auto func1 = [](int val) {
+			cout << val << endl;
+			return val + 1;
+		};
+
+		auto func2 = [](int val) {
 			cout << val << endl;
 		};
 
-		m_tp.AddFunc(func, i);
+		m_result[i] = m_tp.AddFunc(func1, i);
+		m_tp.AddFunc(func2, i);
 	}
 
-	m_tp.AddFunc([&]() { IMAGE->GetVecMesh(L"Player_Idle", L"./Image/obj/thief%df.obj", 40); });
-
-
+	//m_tp.AddFunc([&]() { IMAGE->GetVecMesh(L"Player_Idle", L"./Image/obj/thief%df.obj", 40); });
 }
 
 void TestScene::Update()
 {
 	if (m_tp.isEmpty())
+	{
+		for (auto& iter : m_result)
+			cout << "hi" << iter.get() << endl;
 		SCENE->CHANGESCENE(L"Title");
+	}
 }
 
 void TestScene::Render()

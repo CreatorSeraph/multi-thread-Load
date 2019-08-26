519a5127e9992a785f127940ec65d27ebda6b63f
creatorSeraph <dbgksdl00@gmail.com>
2019-08-27 00:41:45 +0900

    가변템플릿 버젼 타입제한 추가, 버그 수정

--- TestScene.cpp
+++ TestScene.cpp
@@ -13,9 +13,15 @@ void TestScene::Init()
 {
 	for (int i = 0; i < 10; ++i)
 	{
-		m_tp.AddFunc([i]() {
-			cout << i << endl;
-		});
+		//m_tp.AddFunc([i]() {
+		//	cout << i << endl;
+		//});
+
+		auto func = [](int val) {
+			cout << val << endl;
+		};
+
+		m_tp.AddFunc(func, i);
 	}
 
 	m_tp.AddFunc([&]() { IMAGE->GetVecMesh(L"Player_Idle", L"./Image/obj/thief%df.obj", 40); });
